0135e3d87747d068eaf65fb263509a62ba393139
yeeun0118s <yeeun0118s@naver.com>
2024-02-05 17:51:04 +0900

    로그인 구현중

--- src/main/java/shop/mtcoding/blog/user/UserController.java
+++ src/main/java/shop/mtcoding/blog/user/UserController.java
@@ -6,10 +6,26 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestAttribute;
 
+
+/*책임
+    1, 요청처리
+    2. 데이터 유효성 검사
+ */
 @RequiredArgsConstructor //final이 붙은 애들에 대한 생성자를 만들어준다.
 @Controller
 public class UserController {
     private final UserRepository userRepository;
+
+    @PostMapping("/login")
+    public String login(UserRequest.LoginDTO requestDto){
+        System.out.println(requestDto);
+
+        if (requestDto.getUsername().length() < 3){//유효성 검사
+            return "error/400"; //viewResolver 설정이 돼있음.
+        }
+        //userRepository.save2(); // 모델에 위임
+        return null;
+    }
     @PostMapping("/join")
     public String join(UserRequest.JoinDto requestDto) {
         System.out.println(requestDto);
--- src/main/java/shop/mtcoding/blog/user/UserRepository.java
+++ src/main/java/shop/mtcoding/blog/user/UserRepository.java
@@ -5,23 +5,41 @@ import jakarta.persistence.Query;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 
-@Repository
+
+@Repository // IoC에 new할 수 있고, 데이터 베이스 연동
 public class UserRepository {
+
     // DB에 접근할 수 있는 매니저 객체
     // 스프링이 만들어서 IoC에 넣어둔다.
     // DI에서 꺼내 쓰기만 하면된다.
     private EntityManager em;
 
     // 생성자 주입 (DI 코드)
+    //DI:의존성 주입
     public UserRepository(EntityManager em) {
         this.em = em;
     }
-    @Transactional // db에 write 할때는 필수 값을 넣어주는 역할을 하는데 항상 초기화 되도록한다.
-    public void save(UserRequest.JoinDto requestDTO){
-        Query query = em.createNativeQuery("insert into user_tb(username,password,email, created_at)values (?,?,?, now())");
-        query.setParameter(1,requestDTO.getUsername());
-        query.setParameter(2,requestDTO.getPassword());
-        query.setParameter(3,requestDTO.getEmail());
-        query.executeUpdate();// DB로 전송
+
+    @Transactional // 값을 넣어주는 역할을 하는데 항상 초기화 되도록한다.
+    public void save(UserRequest.JoinDto requestDTO) { // Query를 직접 짬
+        Query query = em.createNativeQuery("insert into user_tb (username, password, email) values (?, ?, ?)");
+        query.setParameter(1, requestDTO.getUsername());
+        query.setParameter(2, requestDTO.getPassword());
+        query.setParameter(3, requestDTO.getEmail());
+
+        query.executeUpdate(); // DB로 전송
     }
+
+    @Transactional
+    public User findByUsernameAndPassword(UserRequest.LoginDTO requestDTO){ // 모델 조회 요청
+        Query query = em.createNativeQuery("select * from user_tb where username=? and password=?", User.class);
+        query.setParameter(1, requestDTO.getUsername());
+        query.setParameter(2, requestDTO.getPassword());
+
+        User user =(User) query.getSingleResult(); //DB 조회 결과를 Entity로 받음
+        return user;
+
+    }
+
 }
+
